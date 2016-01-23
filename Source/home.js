e75530fc865386118d0423c937d14a6505d9382c
Andrew Low <andrewjrlow@gmail.com>
2016-01-24 02:46:04 +0800

    new images

--- Source/home.js
+++ Source/home.js
@@ -17,7 +17,7 @@ function getNumber(small, big) {
 var foodList = [
     "Chicken rice",
     "Wanton noodles",
-    "Duck rice", 
+    "Duck rice",
     "Mee siam",
     "Mee goreng",
     "Prata",
@@ -92,13 +92,18 @@ var getRandomFood = function(list){
 };
 
 var imageList = [
-    "http://memesvault.com/wp-content/uploads/Feels-Good-Man-Frog-06.png",
-    "https://static1.fjcdn.com/thumbnails/comments/The+feels+good+meme+_8904f29d2eee2a25fa6c458fba1f8cfb.png",
-    "https://i.ytimg.com/vi/KY5n3Aftass/hqdefault.jpg",
-    "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/2e/2e06f6132a4ffd1d157daac81a8e34148b2e26a3_full.jpg",
-    "http://25.media.tumblr.com/tumblr_mcx4h0P39u1rdd4s9o1_r1_500.png",
-    "https://outsidethewalls.files.wordpress.com/2012/08/cool-jesus.png",
-    "http://justsomething.co/wp-content/uploads/2013/11/guns-replaced-thumbs-up-20.jpg"
+    "blackfeelsgood.png",
+    "chingchong.png",
+    "cool-jesus.png",
+    "feelit.png",
+    "feelsgoodman.png",
+    "Feels-Good-Man-Frog-06.png",
+    "heman.jpg",
+    "impossibru.png",
+    "Gunther.jpg",
+    "stallone.png",
+    "wtfben.png",
+
 ];
 
 $('#getNumber').click(function(){
